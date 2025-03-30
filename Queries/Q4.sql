f5e46ea4c08d0895db581297089eeedea202d149
marcussoo <38975839+marcussoo@users.noreply.github.com>
2025-03-30 21:47:59 +0800

    Added changes

--- Queries/Q4.sql
+++ Queries/Q4.sql
@@ -1,8 +1,15 @@
+/*
+    Question 4: What is the top three most popular first financial goals for investors in 2024?
+ */
+
+DECLARE @Year int;
+SET @Year = 2024;
+
 SELECT TOP 3
     FG.Goal,
     COUNT(*) as GoalCount
 FROM FINANCIAL_GOAL FG, INVESTOR I
-WHERE FG.Phone = I.Phone
-  AND YEAR(FG.CreatedDate) = 2024
+WHERE FG.Phone = I.Phone AND
+      YEAR(FG.CreatedDate) = @Year
 GROUP BY FG.Goal
 ORDER BY GoalCount DESC;
