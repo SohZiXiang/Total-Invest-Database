5d3b9b7b6a0dc1b5cee9e9dc2b79efca75c77aac
Zi Heng <pehz0007@e.ntu.edu.sg>
2025-03-31 01:20:41 +0800

    Fix Query

--- Queries/Q4.sql
+++ Queries/Q4.sql
@@ -8,24 +8,23 @@ WITH FirstGoal AS (
         FG.Goal,
         RANK() OVER (PARTITION BY FG.Phone ORDER BY FG.CreatedDateTime) as Rank
     FROM FINANCIAL_GOAL FG
+    WHERE YEAR(FG.CreatedDateTime) = 2024
 )
-
-SELECT TOP 3
-    FG.Goal,
-    COUNT(*) AS InvestorCount
-FROM INVESTOR I, FirstGoal FG
-WHERE I.Phone = FG.Phone
+SELECT FG.Goal, COUNT(*) AS InvestorCount
+FROM FirstGoal FG
+ WHERE FG.Rank = 1
 GROUP BY FG.Goal
 ORDER BY InvestorCount DESC
+
 
 -- DECLARE @Year int;
 -- SET @Year = 2024;
 --
--- SELECT TOP 3
+-- SELECT
 --     FG.Goal,
 --     COUNT(*) as GoalCount
 -- FROM FINANCIAL_GOAL FG, INVESTOR I
 -- WHERE FG.Phone = I.Phone
---   AND YEAR(FG.CreatedDate) = @Year
+--   AND YEAR(FG.CreatedDateTime) = @Year
 -- GROUP BY FG.Goal
 -- ORDER BY GoalCount DESC;
