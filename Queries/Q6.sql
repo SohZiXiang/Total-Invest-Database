2fc1e3f191cbb87d98f7fe2e732752cf574f1159
Zi Heng <pehz0007@e.ntu.edu.sg>
2025-03-31 00:40:03 +0800

    Fix Query

--- Queries/Q6.sql
+++ Queries/Q6.sql
@@ -2,32 +2,16 @@
     Question 6: Find the most popular financial goals for investors working in the same company and whose age is between 30 to 40 years old.
  */
 
-WITH GoalCounts AS (
-    SELECT
-        I.Company,
-        FG.Goal,
-        COUNT(DISTINCT FG.Phone) AS GoalCount
-    FROM INVESTOR I
-    JOIN FINANCIAL_GOAL FG ON I.Phone = FG.Phone
-    WHERE DATEDIFF(YEAR, I.DoB, GETDATE()) BETWEEN 30 AND 40
-    GROUP BY I.Company, FG.Goal
-),
-MaxGoalCounts AS (
-    SELECT
-        Company,
-        MAX(GoalCount) AS MaxGoalCount
-    FROM GoalCounts
-    GROUP BY Company
+WITH FinancialGoalForEachCompany AS (
+                SELECT DISTINCT I.Company, Goal, COUNT(*) OVER ( PARTITION BY Goal ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS GoalCounts
+                 FROM INVESTOR I, FINANCIAL_GOAL FG
+                 WHERE I.Phone = FG.Phone
+                 AND DATEDIFF(YEAR, I.DoB, GETDATE()) BETWEEN 30 AND 40
 )
-SELECT
-    gc.Company,
-    gc.Goal,
-    gc.GoalCount
-FROM GoalCounts gc
-JOIN MaxGoalCounts mgc
-    ON gc.Company = mgc.Company
-    AND gc.GoalCount = mgc.MaxGoalCount
-ORDER BY gc.Company, gc.GoalCount DESC;
-
-
-
+SELECT Company, Goal, GoalCounts
+FROM (
+    SELECT *,
+           RANK() OVER (PARTITION BY Company ORDER BY GoalCounts DESC) AS rk
+    FROM FinancialGoalForEachCompany
+) AS ranked
+WHERE rk = 1;
