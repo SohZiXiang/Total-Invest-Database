0839669bb5d8157b4f24b5e694430f09fe667371
HX987 <leehsienx@gmail.com>
2025-03-30 23:47:54 +0800

    added 30 more sample data

--- sql_scripts/data/04-INVESTOR.sql
+++ sql_scripts/data/04-INVESTOR.sql
@@ -7,3 +7,41 @@ INSERT INTO master.dbo.INVESTOR (Phone, Email, Name, Gender, DoB, AnnualIncome, 
 INSERT INTO master.dbo.INVESTOR (Phone, Email, Name, Gender, DoB, AnnualIncome, Company) VALUES (N'+65-7345-6789', N'david.lee@email.com', N'David Lee', N'M', N'1992-11-30', 92000.00, N'Tech Solutions Pte Ltd');
 INSERT INTO master.dbo.INVESTOR (Phone, Email, Name, Gender, DoB, AnnualIncome, Company) VALUES (N'+65-6456-7890', N'emma.ng@email.com', N'Emma Ng', N'F', N'1997-04-18', 75000.00, N'Finance Corp Pte Ltd');
 INSERT INTO master.dbo.INVESTOR (Phone, Email, Name, Gender, DoB, AnnualIncome, Company) VALUES (N'+65-5567-8901', N'james.chan@email.com', N'James Chan', N'M', N'1993-09-25', 88000.00, N'Investment Bank Pte Ltd');
+
+-- Additional 30 Investors (2021-2024)
+INSERT INTO master.dbo.INVESTOR (Phone, Email, Name, Gender, DoB, AnnualIncome, Company) VALUES
+-- 2021 Investors
+(N'+65-8123-4568', N'james.tan@techsolutions.com', N'James Tan', N'M', N'1990-03-15', 85000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-9123-4569', N'lisa.wong@financecorp.com', N'Lisa Wong', N'F', N'1992-06-20', 92000.00, N'Finance Corp Pte Ltd'),
+(N'+65-8234-5670', N'robert.lee@investmentbank.com', N'Robert Lee', N'M', N'1988-09-10', 95000.00, N'Investment Bank Pte Ltd'),
+(N'+65-9234-5671', N'michelle.chen@techsolutions.com', N'Michelle Chen', N'F', N'1995-12-05', 78000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-8345-6782', N'thomas.lim@financecorp.com', N'Thomas Lim', N'M', N'1991-12-20', 88000.00, N'Finance Corp Pte Ltd'),
+(N'+65-9345-6783', N'jennifer.tan@investmentbank.com', N'Jennifer Tan', N'F', N'1993-12-31', 92000.00, N'Investment Bank Pte Ltd'),
+(N'+65-8456-7894', N'william.wong@techsolutions.com', N'William Wong', N'M', N'1989-12-31', 95000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-9456-7895', N'rachel.lee@financecorp.com', N'Rachel Lee', N'F', N'1994-12-31', 85000.00, N'Finance Corp Pte Ltd'),
+(N'+65-8567-8906', N'daniel.chen@investmentbank.com', N'Daniel Chen', N'M', N'1990-12-31', 98000.00, N'Investment Bank Pte Ltd'),
+(N'+65-9567-8907', N'emma.lim@techsolutions.com', N'Emma Lim', N'F', N'1996-12-31', 82000.00, N'Tech Solutions Pte Ltd'),
+
+-- 2022 Investors
+(N'+65-8678-9018', N'andrew.tan@techsolutions.com', N'Andrew Tan', N'M', N'1991-01-15', 88000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-9678-9019', N'sophia.wong@financecorp.com', N'Sophia Wong', N'F', N'1993-04-20', 92000.00, N'Finance Corp Pte Ltd'),
+(N'+65-8789-0120', N'benjamin.lee@investmentbank.com', N'Benjamin Lee', N'M', N'1989-07-10', 95000.00, N'Investment Bank Pte Ltd'),
+(N'+65-9789-0121', N'isabella.chen@techsolutions.com', N'Isabella Chen', N'F', N'1995-10-05', 80000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-8890-1232', N'christopher.lim@financecorp.com', N'Christopher Lim', N'M', N'1992-12-20', 90000.00, N'Finance Corp Pte Ltd'),
+(N'+65-9890-1233', N'olivia.tan@investmentbank.com', N'Olivia Tan', N'F', N'1994-12-31', 94000.00, N'Investment Bank Pte Ltd'),
+(N'+65-8901-2344', N'joseph.wong@techsolutions.com', N'Joseph Wong', N'M', N'1990-12-31', 96000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-9901-2345', N'victoria.lee@financecorp.com', N'Victoria Lee', N'F', N'1993-12-31', 86000.00, N'Finance Corp Pte Ltd'),
+(N'+65-9012-3456', N'alexander.chen@investmentbank.com', N'Alexander Chen', N'M', N'1988-12-31', 99000.00, N'Investment Bank Pte Ltd'),
+(N'+65-9012-3457', N'grace.lim@techsolutions.com', N'Grace Lim', N'F', N'1997-12-31', 83000.00, N'Tech Solutions Pte Ltd'),
+
+-- 2023 Investors
+(N'+65-8123-4560', N'ryan.tan@techsolutions.com', N'Ryan Tan', N'M', N'1992-02-15', 89000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-9123-4561', N'chloe.wong@financecorp.com', N'Chloe Wong', N'F', N'1994-05-20', 93000.00, N'Finance Corp Pte Ltd'),
+(N'+65-8234-5672', N'marcus.lee@investmentbank.com', N'Marcus Lee', N'M', N'1990-08-10', 96000.00, N'Investment Bank Pte Ltd'),
+(N'+65-9234-5673', N'ava.chen@techsolutions.com', N'Ava Chen', N'F', N'1996-11-05', 81000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-8345-6784', N'lucas.lim@financecorp.com', N'Lucas Lim', N'M', N'1993-12-20', 91000.00, N'Finance Corp Pte Ltd'),
+(N'+65-9345-6785', N'lily.tan@investmentbank.com', N'Lily Tan', N'F', N'1995-12-31', 95000.00, N'Investment Bank Pte Ltd'),
+(N'+65-8456-7896', N'henry.wong@techsolutions.com', N'Henry Wong', N'M', N'1991-12-31', 97000.00, N'Tech Solutions Pte Ltd'),
+(N'+65-9456-7897', N'zoe.lee@financecorp.com', N'Zoe Lee', N'F', N'1994-12-31', 87000.00, N'Finance Corp Pte Ltd'),
+(N'+65-8567-8908', N'leo.chen@investmentbank.com', N'Leo Chen', N'M', N'1989-12-31', 100000.00, N'Investment Bank Pte Ltd'),
+(N'+65-9567-8909', N'mia.lim@techsolutions.com', N'Mia Lim', N'F', N'1998-12-31', 84000.00, N'Tech Solutions Pte Ltd');
--- sql_scripts/data/09-FINANCIAL_GOAL.sql
+++ sql_scripts/data/09-FINANCIAL_GOAL.sql
@@ -7,3 +7,41 @@ INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDat
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-7345-6789', N'Start Business', 500000.00, 3, '2024-04-01');
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-6456-7890', N'Buy Car', 150000.00, 2, '2024-05-13');
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-5567-8901', N'Wedding Fund', 100000.00, 1, '2024-07-18');
+
+-- Additional 30 Financial Goals (2021-2024)
+INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES
+-- 2021 Financial Goals
+(N'+65-8123-4568', N'Buy House', 600000.00, 5, '2021-03-15'),
+(N'+65-9123-4569', N'Retirement', 1200000.00, 20, '2021-06-20'),
+(N'+65-8234-5670', N'Start Business', 250000.00, 3, '2021-09-10'),
+(N'+65-9234-5671', N'Children''s Education', 350000.00, 10, '2021-12-05'),
+(N'+65-8345-6782', N'Travel Fund', 75000.00, 2, '2021-12-20'),
+(N'+65-9345-6783', N'Buy Car', 150000.00, 3, '2021-12-31'),
+(N'+65-8456-7894', N'Emergency Fund', 100000.00, 1, '2021-12-31'),
+(N'+65-9456-7895', N'Wedding Fund', 200000.00, 4, '2021-12-31'),
+(N'+65-8567-8906', N'Home Renovation', 180000.00, 3, '2021-12-31'),
+(N'+65-9567-8907', N'Investment Portfolio', 400000.00, 5, '2021-12-31'),
+
+-- 2022 Financial Goals
+(N'+65-8678-9018', N'Buy House', 700000.00, 5, '2022-01-15'),
+(N'+65-9678-9019', N'Retirement', 1500000.00, 20, '2022-04-20'),
+(N'+65-8789-0120', N'Start Business', 300000.00, 3, '2022-07-10'),
+(N'+65-9789-0121', N'Children''s Education', 400000.00, 10, '2022-10-05'),
+(N'+65-8890-1232', N'Travel Fund', 100000.00, 2, '2022-12-20'),
+(N'+65-9890-1233', N'Buy Car', 180000.00, 3, '2022-12-31'),
+(N'+65-8901-2344', N'Emergency Fund', 120000.00, 1, '2022-12-31'),
+(N'+65-9901-2345', N'Wedding Fund', 250000.00, 4, '2022-12-31'),
+(N'+65-9012-3456', N'Home Renovation', 200000.00, 3, '2022-12-31'),
+(N'+65-9012-3457', N'Investment Portfolio', 500000.00, 5, '2022-12-31'),
+
+-- 2023 Financial Goals
+(N'+65-8123-4560', N'Buy House', 800000.00, 5, '2023-02-15'),
+(N'+65-9123-4561', N'Retirement', 1800000.00, 20, '2023-05-20'),
+(N'+65-8234-5672', N'Start Business', 350000.00, 3, '2023-08-10'),
+(N'+65-9234-5673', N'Children''s Education', 450000.00, 10, '2023-11-05'),
+(N'+65-8345-6784', N'Travel Fund', 125000.00, 2, '2023-12-20'),
+(N'+65-9345-6785', N'Buy Car', 200000.00, 3, '2023-12-31'),
+(N'+65-8456-7896', N'Emergency Fund', 150000.00, 1, '2023-12-31'),
+(N'+65-9456-7897', N'Wedding Fund', 300000.00, 4, '2023-12-31'),
+(N'+65-8567-8908', N'Home Renovation', 250000.00, 3, '2023-12-31'),
+(N'+65-9567-8909', N'Investment Portfolio', 600000.00, 5, '2023-12-31');
