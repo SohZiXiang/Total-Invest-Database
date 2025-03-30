1a7f7763f0c5bf908615bcc96ff35cfa9bf246d8
marcussoo <38975839+marcussoo@users.noreply.github.com>
2025-03-30 23:14:51 +0800

    Updated financial_goal database

--- sql_scripts/data/09-FINANCIAL_GOAL.sql
+++ sql_scripts/data/09-FINANCIAL_GOAL.sql
@@ -1,10 +1,8 @@
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-9111-2222', N'Buy House', 500000.00, 5, '2024-03-30');
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-9111-2222', N'Retirement', 1000000.00, 20, '2024-10-24');
-
--- 2024 Financial Goals Data
-INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-9123-4567', N'Buy Condo', 800000.00, 5, '2024-02-15');
-INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-8234-5678', N'Early Retirement', 2000000.00, 15, '2024-01-01');
+INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-9123-4567', N'Buy House', 800000.00, 5, '2024-02-15');
+INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-8234-5678', N'Retirement', 2000000.00, 15, '2024-01-01');
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-7345-6789', N'Start Business', 500000.00, 3, '2024-04-01');
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-6456-7890', N'Buy Car', 150000.00, 2, '2024-05-13');
 INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-5567-8901', N'Wedding Fund', 100000.00, 1, '2024-07-18');
-INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-7345-6789', N'Buy Condo', 800000.00, 5, '2024-03-20');
+INSERT INTO master.dbo.FINANCIAL_GOAL (Phone, Goal, Amount, Timeline, CreatedDate) VALUES (N'+65-7345-6789', N'Buy House', 800000.00, 5, '2024-03-20');
