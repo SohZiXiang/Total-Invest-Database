3b10f80921f68988d91e39205810f3e645f7ce56
HX987 <leehsienx@gmail.com>
2025-03-31 00:35:27 +0800

    updated the data

--- sql_scripts/data/10-RISK_TOLERANCE.sql
+++ sql_scripts/data/10-RISK_TOLERANCE.sql
@@ -1,1 +1,47 @@
 INSERT INTO master.dbo.RISK_TOLERANCE (Phone, RiskLevel, Q1Answer, Q2Answer, Q3Answer, Q4Answer, Q5Answer) VALUES (N'+65-9111-2222', N'Moderate', 3, 2, 4, 3, 2);
+
+-- Additional Risk Tolerance Data for All Investors
+INSERT INTO master.dbo.RISK_TOLERANCE (Phone, RiskLevel, Q1Answer, Q2Answer, Q3Answer, Q4Answer, Q5Answer) VALUES
+-- 2024 Investors
+(N'+65-9222-3333', N'Conservative', 2, 1, 3, 2, 1),
+(N'+65-9123-4567', N'Aggressive', 4, 5, 4, 5, 4),
+(N'+65-8234-5678', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-7345-6789', N'Aggressive', 5, 4, 5, 4, 5),
+(N'+65-6456-7890', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-5567-8901', N'Moderate', 3, 3, 3, 3, 3),
+
+-- 2021 Investors
+(N'+65-8123-4568', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-9123-4569', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-8234-5670', N'Aggressive', 4, 5, 4, 5, 4),
+(N'+65-9234-5671', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-8345-6782', N'Conservative', 2, 1, 2, 1, 2),
+(N'+65-9345-6783', N'Aggressive', 5, 4, 5, 4, 5),
+(N'+65-8456-7894', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-9456-7895', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-8567-8906', N'Aggressive', 4, 5, 4, 5, 4),
+(N'+65-9567-8907', N'Moderate', 3, 3, 3, 3, 3),
+
+-- 2022 Investors
+(N'+65-8678-9018', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-9678-9019', N'Aggressive', 4, 5, 4, 5, 4),
+(N'+65-8789-0120', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-9789-0121', N'Conservative', 2, 1, 2, 1, 2),
+(N'+65-8890-1232', N'Aggressive', 5, 4, 5, 4, 5),
+(N'+65-9890-1233', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-8901-2344', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-9901-2345', N'Aggressive', 4, 5, 4, 5, 4),
+(N'+65-9012-3456', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-9012-3457', N'Conservative', 2, 1, 2, 1, 2),
+
+-- 2023 Investors
+(N'+65-8123-4560', N'Aggressive', 5, 4, 5, 4, 5),
+(N'+65-9123-4561', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-8234-5672', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-9234-5673', N'Aggressive', 4, 5, 4, 5, 4),
+(N'+65-8345-6784', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-9345-6785', N'Conservative', 2, 1, 2, 1, 2),
+(N'+65-8456-7896', N'Aggressive', 5, 4, 5, 4, 5),
+(N'+65-9456-7897', N'Moderate', 3, 3, 3, 3, 3),
+(N'+65-8567-8908', N'Conservative', 2, 2, 2, 2, 2),
+(N'+65-9567-8909', N'Aggressive', 4, 5, 4, 5, 4);
