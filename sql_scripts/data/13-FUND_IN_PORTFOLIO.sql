ef9360a2fd44011d7ab526e01e105fd3b05f79ce
Soh Zi Xiang <soh.zixiang01@gmail.com>
2025-04-01 15:46:42 +0800

    Fixed post trade co to show companies instead of decimals

--- sql_scripts/data/13-FUND_IN_PORTFOLIO.sql
+++ sql_scripts/data/13-FUND_IN_PORTFOLIO.sql
@@ -1,15 +1,18 @@
+-- 2023 Fund Portfolio Data
 SET IDENTITY_INSERT master.dbo.FUND_IN_PORTFOLIO ON;
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone)VALUES (2, N'2023-03-01', 0.2000, 10000.00, 5, 1, N'555-111-2222');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (3, N'2023-03-10', 0.1500, 7500.00, 6, 1, N'555-111-2222');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (4, N'2023-04-05', 0.2500, 18750.00, 5, 2, N'555-222-3333');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (5, N'2023-04-20', 0.1000, 7500.00, 6, 2, N'555-222-3333');
-SET IDENTITY_INSERT master.dbo.FUND_IN_PORTFOLIO OFF;
+INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone)
+VALUES
+    (2, N'2023-03-01', 0.2000, 'BlackRock', 5, 1, N'555-111-2222'),
+    (3, N'2023-03-10', 0.1500, 'Vanguard', 6, 1, N'555-111-2222'),
+    (4, N'2023-04-05', 0.2500, 'State Street', 5, 2, N'555-222-3333'),
+    (5, N'2023-04-20', 0.1000, 'Fidelity Investments', 6, 2, N'555-222-3333');
 
 -- 2024 Fund Portfolio Data
-SET IDENTITY_INSERT master.dbo.FUND_IN_PORTFOLIO ON;
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (6, N'2024-01-15', 0.3000, 30000.00, 5, 3, N'+65-9123-4567');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (7, N'2024-01-30', 0.3000, 45000.00, 6, 4, N'+65-8234-5678');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (8, N'2024-02-15', 0.4000, 80000.00, 5, 5, N'+65-7345-6789');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (9, N'2024-03-01', 0.4000, 70000.00, 6, 6, N'+65-6456-7890');
-INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (10, N'2024-03-15', 0.3000, 75000.00, 5, 7, N'+65-5567-8901');
+INSERT INTO master.dbo.FUND_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone)
+VALUES
+    (6, N'2024-01-15', 0.3000, 'J.P. Morgan Asset Management', 5, 3, N'+65-9123-4567'),
+    (7, N'2024-01-30', 0.3000, 'PIMCO', 6, 4, N'+65-8234-5678'),
+    (8, N'2024-02-15', 0.4000, 'BNY Mellon', 5, 5, N'+65-7345-6789'),
+    (9, N'2024-03-01', 0.4000, 'Northern Trust', 6, 6, N'+65-6456-7890'),
+    (10, N'2024-03-15', 0.3000, 'HSBC Global Asset Management', 5, 7, N'+65-5567-8901');
 SET IDENTITY_INSERT master.dbo.FUND_IN_PORTFOLIO OFF;
--- sql_scripts/data/14-STOCK_IN_PORTFOLIO.sql
+++ sql_scripts/data/14-STOCK_IN_PORTFOLIO.sql
@@ -1,15 +1,17 @@
 SET IDENTITY_INSERT master.dbo.STOCK_IN_PORTFOLIO ON;
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone)VALUES (2, N'2023-01-15', 0.3000, 15000.00, 1, 1, N'+65-9111-2222');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (3, N'2023-01-20', 0.2000, 10000.00, 2, 1, N'+65-9111-2222');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (4, N'2023-02-10', 0.2500, 18750.00, 1, 2, N'+65-9222-3333');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (5, N'2023-02-15', 0.1500, 11250.00, 2, 2, N'+65-9222-3333');
-SET IDENTITY_INSERT master.dbo.STOCK_IN_PORTFOLIO OFF;
+INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone)
+VALUES
+    (2, N'2023-01-15', 0.3000, 'Saxo Bank', 1, 1, N'+65-9111-2222'),
+    (3, N'2023-01-20', 0.2000, 'Interactive Brokers', 2, 1, N'+65-9111-2222'),
+    (4, N'2023-02-10', 0.2500, 'Clearstream', 1, 2, N'+65-9222-3333'),
+    (5, N'2023-02-15', 0.1500, 'Charles Schwab', 2, 2, N'+65-9222-3333');
 
 -- 2024 Stock Portfolio Data
-SET IDENTITY_INSERT master.dbo.STOCK_IN_PORTFOLIO ON;
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (6, N'2024-01-05', 0.4000, 40000.00, 1, 3, N'+65-9123-4567');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (7, N'2024-01-20', 0.3000, 45000.00, 2, 4, N'+65-8234-5678');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (8, N'2024-02-05', 0.3500, 70000.00, 1, 5, N'+65-7345-6789');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (9, N'2024-02-20', 0.2500, 43750.00, 2, 6, N'+65-6456-7890');
-INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone) VALUES (10, N'2024-03-05', 0.4000, 100000.00, 1, 7, N'+65-5567-8901');
+INSERT INTO master.dbo.STOCK_IN_PORTFOLIO (ID, StartDate, AllocationRatio, PostTradeCO, AssetID, PID, Phone)
+VALUES
+    (6, N'2024-01-05', 0.4000, 'Fidelity', 1, 3, N'+65-9123-4567'),
+    (7, N'2024-01-20', 0.3000, 'Morgan Stanley', 2, 4, N'+65-8234-5678'),
+    (8, N'2024-02-05', 0.3500, 'Goldman Sachs', 1, 5, N'+65-7345-6789'),
+    (9, N'2024-02-20', 0.2500, 'Deutsche Bank', 2, 6, N'+65-6456-7890'),
+    (10, N'2024-03-05', 0.4000, 'UBS', 1, 7, N'+65-5567-8901');
 SET IDENTITY_INSERT master.dbo.STOCK_IN_PORTFOLIO OFF;
